c79e98fd6cb0ca6b7b070cf702bfd9d6749fbcd8
Mạnh Thắng <135790146+manhthang0106@users.noreply.github.com>
2025-11-18 16:16:56 +0700

    Initial MVP setup with all features

---
+++ scripts/constants.ts
@@ -0,0 +1,7 @@
+export const ROOT_DIR = process.env.ROOT_DIR || './'
+export const STREAMS_DIR = process.env.STREAMS_DIR || './streams'
+export const OUTPUT_DIR = process.env.OUTPUT_DIR || './output'
+export const API_DIR = process.env.API_DIR || './.api'
+export const LOGS_DIR = process.env.LOGS_DIR || './logs'
+export const TESTING = process.env.NODE_ENV === 'test' ? true : false
+export const EOL = '\r\n'
